d04fa55911dfdfef309bda7f340c5499c1923b42
Yash Singh <iamyashsinghh@gmail.com>
2024-12-05 10:43:23 +0530

    ok

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -16,7 +16,7 @@ class AdminAuth
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (!Auth::guard('admin')->check()) {
+        if (!Auth::guard('admin')->check()) { 
             return redirect()->route('login');
         }
         return $next($request);
