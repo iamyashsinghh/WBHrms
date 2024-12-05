dce5fe8ad217a57e69007ecda2dbbf6c381441dc
Yash Singh <iamyashsinghh@gmail.com>
2024-12-05 10:51:41 +0530

    ok

--- app/Http/Middleware/BackGroundAuth.php
+++ app/Http/Middleware/BackGroundAuth.php
@@ -13,7 +13,7 @@ class BackGroundAuth
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
+     */ 
     public function handle(Request $request, Closure $next): Response
     {
         if (!Auth::guard('background')->check()) {
