d359e0df42980408f8868a5dbb543708256d1809
Yash Singh <iamyashsinghh@gmail.com>
2024-12-05 10:47:16 +0530

    ok

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
 echo "Deployment started..."
+
 git pull
+
 echo "New changes copied to server !"
+
 echo "Installing Dependencies..."
+
 composer8.2 install
+
 echo "Some Artisan Commands..."
+
 php8.2 artisan migrate
+
 php8.2 artisan storage:link
+
 echo "Deployment Finished!"
