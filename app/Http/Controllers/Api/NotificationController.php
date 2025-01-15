da6a5c707b8a726c75a1fe8bdcc174bf93e9a588
Yash Singh <iamyashsinghh@gmail.com>
2025-01-15 14:53:49 +0530

    ok

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Attendance;
 use App\Models\Notification;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Request;
 
 class NotificationController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $user = $request->user();
         $notifications = Notification::orderBy('created_at', 'desc')->get();
-
+        $attendance = Attendance::where('date', Carbon::today()->toDateString())->where('emp_code', $user->emo_code);
         return response()->json([
             'notifications' => $notifications,
+            'attendance' => $attendance,
         ]);
     }
 }
