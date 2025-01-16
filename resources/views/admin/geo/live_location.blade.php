41029359898e26ec7e6242ce8ad3a0c79020411a
Yash Singh <iamyashsinghh@gmail.com>
2025-01-16 11:38:40 +0530

    ok

--- resources/views/admin/geo/live_location.blade.php
+++ resources/views/admin/geo/live_location.blade.php
@@ -111,7 +111,7 @@
             <br> Punch In at: ${location.punch_in_time ? moment(location.punch_in_time, 'HH:mm:ss').format('h:mm:ss a') : 'N/A'}
             <br> Punch Out at: ${location.punch_out_time ? moment(location.punch_out_time, 'HH:mm:ss').format('h:mm:ss a') : 'N/A'}
             <br>   <div style="display: flex; justify-content: space-between; align-items: center;">
-        <div>View History: <a href="${history_url || '#'}" target="_blank" rel="noopener noreferrer">View</a></div>
+        <div>History: <a href="${history_url || '#'}" target="_blank" rel="noopener noreferrer">View</a></div>
         <button
             style="background: none; border: none; color: #891010; cursor: pointer; margin-left: 10px;"
             onclick="zoomToLocation(${location.latitude}, ${location.longitude})"
