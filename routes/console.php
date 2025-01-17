8747caaf3cce24662c10e813552de81219a2fc9e
Yash Singh <iamyashsinghh@gmail.com>
2025-01-17 11:22:48 +0530

    ok

--- routes/console.php
+++ routes/console.php
@@ -2,11 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 
-Artisan::command('app:send-daily-notification-to-user-for-attendance', function(){
-    \Illuminate\Support\Facades\Artisan::call('app:send-daily-notification-to-user-for-attendance');
-})->purpose('Send FCM Notification to employees')->everyMinute();
+Schedule::command('app:send-daily-notification-to-user-for-attendance')->purpose('Send FCM Notification to employees')->everyMinute();
