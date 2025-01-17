0a91158671b5cb3731b83b34fc1e9fb4f31ee152
Yash Singh <iamyashsinghh@gmail.com>
2025-01-17 11:25:12 +0530

    ok

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ use Illuminate\Support\Facades\Schedule;
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('app:send-daily-notification-to-user-for-attendance')->purpose('Send FCM Notification to employees')->everyMinute();
+Schedule::command('app:send-daily-notification-to-user-for-attendance')->everyMinute();
