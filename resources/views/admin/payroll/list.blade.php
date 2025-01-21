ce190ba5346731de454ff4bf6f6226b4378c8873
Yash Singh <iamyashsinghh@gmail.com>
2025-01-21 15:33:25 +0530

    ok

--- resources/views/admin/payroll/list.blade.php
+++ resources/views/admin/payroll/list.blade.php
@@ -68,19 +68,22 @@
                 </table>
             </div>
         </section>
-        <div class="modal fade" id="pdfModal" tabindex="-1" aria-labelledby="pdfModalLabel" aria-hidden="true">
+        <div class="modal fade" id="pdfModal" tabindex="-1" aria-labelledby="viewDocumentModalLabel"
+        aria-hidden="true">
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="pdfModalLabel">PDF Preview</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
+                        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        </div>
                     <div class="modal-body">
                         <iframe id="pdfPreview" src="" frameborder="0" style="width: 100%; height: 600px;"></iframe>
                     </div>
                     <div class="modal-footer">
+                        <a id="downloadPdf" href="#" target="_blank" class="btn btn-primary" download>Download</a>
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <a id="downloadPdf" href="#" target="_blank" class="btn btn-primary">Download</a>
                     </div>
                 </div>
             </div>
